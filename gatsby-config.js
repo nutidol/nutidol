9b7cb52d7a71af056df1a1e150392b5ac770d02a
nattharikast <nutidol@gmail.com>
2023-09-25 12:40:35 +0100

    path fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 const path = require(`path`)
 
 module.exports = {
-  pathPrefix: `/nst`,
+  pathPrefix: `/nutidol`,
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
